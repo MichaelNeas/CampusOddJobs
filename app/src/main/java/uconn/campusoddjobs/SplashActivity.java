7e85bde561ee70a99c9ae66566517beb687c1550
ejh12002 <edward.hanlon@uconn.edu>
2015-03-04 19:42:13 -0500

    Initial commit

---
+++ app/src/main/java/uconn/campusoddjobs/SplashActivity.java
@@ -0,0 +1,57 @@
+package uconn.campusoddjobs;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Intent;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    private static String TAG = SplashActivity.class.getName();
+    private static long SLEEP_TIME = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        // Start timer and launch main activity
+        IntentLauncher launcher = new IntentLauncher();
+        launcher.start();
+    }
+
+    private class IntentLauncher extends Thread {
+        @Override
+        /**
+         * Sleep for some time and than start new activity.
+         */
+        public void run() {
+            try {
+                // Sleeping
+                Thread.sleep(SLEEP_TIME*1000);
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+
+            // Start main activity
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            SplashActivity.this.startActivity(intent);
+            SplashActivity.this.finish();
+        }
+    }
+}
