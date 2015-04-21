a2ed526ca7346aae296acc939c3c9cf7c6aa4e5c
ejh12002 <edward.hanlon@uconn.edu>
2015-04-21 17:04:03 -0400

    login and registration (email+password only) functions linked to database

--- app/src/main/java/uconn/campusoddjobs/Register.java
+++ app/src/main/java/uconn/campusoddjobs/Register.java
@@ -1,15 +1,26 @@
 package uconn.campusoddjobs;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class Register extends Activity {
 
@@ -20,25 +31,40 @@ public class Register extends Activity {
     private EditText username;
     private EditText rPasswordView;
 
+    private Button rRegisterButton;
+    private Button rBackToLoginButton;
+
+    private ProgressDialog pDialog;
+
+    private static final String LOGIN_URL = "http://campusoddjobs.com/oddjobs/register.php";
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    JSONparser jsonParser = new JSONparser();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // currently only email and password are registered
+        rEmailView = (EditText) findViewById(R.id.email);
+        rPasswordView = (EditText) findViewById(R.id.pword);
+        // TODO implement the remaining elements in the database (and make a column for karma)
         fname = (EditText) findViewById(R.id.fname);
         lname = (EditText) findViewById(R.id.lname);
-        rEmailView = (EditText) findViewById(R.id.email);
         username = (EditText) findViewById(R.id.uname);
-        rPasswordView = (EditText) findViewById(R.id.password);
 
-        Button rRegisterButton = (Button) findViewById(R.id.register);
+        rRegisterButton = (Button) findViewById(R.id.register);
         rRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                attemptRegistration();
+            public void onClick(View view){
+                new CreateUser().execute();
             }
         });
-        Button rBackToLoginButton = (Button) findViewById(R.id.bktologin);
+
+        rBackToLoginButton = (Button) findViewById(R.id.bktologin);
         rBackToLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,16 +73,81 @@ public class Register extends Activity {
         });
     }
 
-    private void attemptRegistration() {
-        // some magical database shit happens here
-    }
-
-    private void toLogin(){
-        Intent intent = new Intent(Register.this,LoginActivity.class);
+    private void toLogin() {
+        Intent intent = new Intent(Register.this, LoginActivity.class);
         Register.this.startActivity(intent);
         Register.this.finish();
     }
 
+    class CreateUser extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Register.this);
+            pDialog.setMessage("Creating User...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // Check for success tag
+            int success;
+            String email = rEmailView.getText().toString();
+            String password = rPasswordView.getText().toString();
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("email", email));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+
+                //Posting user data to script
+                JSONObject json = jsonParser.makeHttpRequest(
+                        LOGIN_URL, "POST", params);
+
+                // full json response
+                Log.d("Login attempt", json.toString());
+
+                // json success element
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("User Created!", json.toString());
+                    finish();
+                    return json.getString(TAG_MESSAGE);
+                } else {
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * *
+         */
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null) {
+                Toast.makeText(Register.this, file_url, Toast.LENGTH_LONG).show();
+            }
 
 
+        }
+    }
 }
